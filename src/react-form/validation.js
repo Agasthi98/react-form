f9ad0b2ce0978be2d1292e339c4bddf6e1b65ccb
Agasthi98 <sankalana98@gmail.com>
2023-11-03 19:09:23 +0530

    validate username

--- src/react-form/validation.js
+++ src/react-form/validation.js
@@ -1,7 +1,9 @@
 const Validation = (values) => {
+  const usernameRegex = /^[a-zA-Z]+$/;
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const isEmailValid = emailRegex.test(values.email);
+  const isUserNameValid = usernameRegex.test(values.username);
   const isPasswordValid = values.password.length >= 8;
-  return isEmailValid && isPasswordValid;
+  return isUserNameValid && isEmailValid && isPasswordValid;
 };
 export default Validation;
